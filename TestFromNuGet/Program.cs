e3b9f42f9b3f215a84833643b29482ea821d031b
edgar <edgar@SAWAYAMA>
2019-08-19 12:03:30 +0900

    First Commit

---
+++ TestFromNuGet/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wrapper;
+
+namespace TestFromNuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+
+            Console.WriteLine("Test for a Nuget Package...\n");
+
+            Loader.Test();
+
+            Console.ReadKey();
+
+           
+
+        }
+    }
+}
